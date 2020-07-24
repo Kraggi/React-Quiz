7679e47d30e75435a09ce498b4414afb233d4ec3
German Yanovich <germanyanovich@gmail.com>
2020-07-24 03:54:38 +0300

    added Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Layout from './hoc/Layout/Layout';
 
 function App() {
   return (
-    <div>
-      <h1>Initialize</h1>
-    </div>
+    <Layout>
+      <div style={{ width: '400px', border: '5px solid #ccc' }}>
+        <h1>Hello Layout</h1>
+      </div>
+    </Layout>
   );
 }
 
