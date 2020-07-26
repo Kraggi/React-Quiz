9bcdd9f9e56286ee5063f7014596cd68af7a5f6d
German Yanovich <germanyanovich@gmail.com>
2020-07-26 09:19:41 +0300

    added login form

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.scss';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 export default class Auth extends Component {
+  loginHandler = () => {};
+  registerHandler = () => {};
+  submitHandler = e => {
+    e.preventDefault();
+  };
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form className={classes.Auth_Form} onSubmit={this.submitHandler}>
+            <Input label='Email' />
+            <Input label='Пароль' errorMessage={'Неправильный пароль'} />
+            <Button type='success' onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type='primary' onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
